db957a95f8c509274de56303e46e85e976289d71
muyeon <gmakin36@gmail.com>
2024-10-17 18:13:43 +0900

    style: 인풋 섹션 스타일 추가

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,9 +4,9 @@ import CustomInput from "@/components/CustomInput/CustomInput";
 export default function RootPage() {
   return (
     <div className="flex flex-col mx-auto w-full">
-      <section>
-        <CustomInput />
-        <CustomButton label="입력" />
+      <section className="flex items-center w-full">
+        <CustomInput className="flex-grow mr-2" />
+        <CustomButton label="입력" className="flex-shrink-0" />
       </section>
       page
     </div>
